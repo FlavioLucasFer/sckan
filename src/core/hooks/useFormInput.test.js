6bbac9c389efec3ed09e963df54556ca3253e259
FlavioLucasFer <flaviolukfer@gmail.com>
2021-06-30 11:10:29 -0300

    Changed package structure, implemented useForm Input hook and it own test

---
+++ src/core/hooks/useFormInput.test.js
@@ -0,0 +1,58 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useFormInput from './useFormInput';
+
+describe('Testing useFormInput custom hook', () => {
+	test('Should have "teste" as default value', () => {
+		const { result } = renderHook(() => useFormInput('test'));
+
+		expect(result.current.value).toBe('test');
+	});
+
+	test('Should change value to "testando"', () => {
+		const { result } = renderHook(() => useFormInput('test'));
+		const e = {
+			target: {
+				value: 'onchange',
+			},
+		};
+
+		expect(result.current.value).toBe('test');
+
+		act(() => result.current.onChange(e));
+
+		expect(result.current.value).toBe('onchange');
+	});
+
+	test('Should value be defined', () => {
+		const { result } = renderHook(() => useFormInput('0'));
+
+		expect(result.current.value).toBeDefined();
+	});
+
+	test('Should value be undefined', () => {
+		const { result } = renderHook(() => useFormInput());
+
+		expect(result.current.value).toBeUndefined();
+	});
+
+	test('Should onChange be a function', () => {
+		const { result } = renderHook(() => useFormInput());
+
+		expect(typeof result.current.onChange).toBe('function');
+	});
+
+	test('The value must start undefined, but become defined on change', () => {
+		const { result } = renderHook(() => useFormInput());
+		const e = {
+			target: {
+				value: 'test',
+			},
+		};
+
+		expect(result.current.value).toBeUndefined();
+
+		act(() => result.current.onChange(e));
+
+		expect(result.current.value).toBeDefined();
+	});
+});
