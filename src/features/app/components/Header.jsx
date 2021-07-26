61c92d78830cb5558318c349e48ca71f3916de96
FlavioLucasFer <flaviolukfer@gmail.com>
2021-07-26 20:20:18 -0300

    Restructured header

--- src/features/app/components/Header.jsx
+++ src/features/app/components/Header.jsx
@@ -1,35 +1,44 @@
+import './Header.css';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { Navbar, NavItem, Icon } from 'react-materialize';
+import { useHistory } from 'react-router-dom';
 
-import { SECUNDARY_COLOR } from 'core/utils/constants';
-import { LOGIN_ROUTE } from 'core/utils/routes';
-
-import SideNavBar from './SideNavBar';
+import Icon from 'core/components/Icon';
+import { HOME_ROUTE } from 'core/utils/routes';
 
 function Header() {
-	const { pathname } = useLocation();
+	const { push } = useHistory();
 
-	if (pathname != LOGIN_ROUTE) {
-		return (
-			<Navbar alignLinks="right"
-				data-testid='navbar'
-				style={{ height: '50px', lineHeight: '50px', backgroundColor: SECUNDARY_COLOR }}
-				menuIcon={
-					<Icon id='open-menu-icon' large>
-						{window.innerWidth > 768 ? 'view_sidebar' : 'menu'}
-					</Icon>
-				}
-				sidenav={<SideNavBar />}
-				options={{
-					edge: 'left',
-					preventScrolling: true
-				}} >
-			</Navbar>
-		);
+	function handleLogoClick() {
+		push(HOME_ROUTE);
 	}
 
-	return null;
+	return (
+		<nav data-testid="navbar" className="white" style={{ height: '50px' }}>
+			<div className="nav-wrapper">
+				<a data-testid="app-logo" className="brand-logo center grey-text text-darken-3"
+					onClick={handleLogoClick}>
+					Sckan
+				</a>
+				<ul className="right hide-on-med-and-down nav-ul">
+					<li data-testid="user-account">
+						<Icon type="round" 
+							size="small"
+							color="grey-text text-darken-3">
+							account_circle
+						</Icon>
+					</li>
+
+					<li data-testid="more-options">
+						<Icon type="round"
+							size="small"
+							color="grey-text text-darken-3">
+							more_vert
+						</Icon>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
--- src/features/app/components/Header.test.jsx
+++ src/features/app/components/Header.test.jsx
@@ -1,40 +1,72 @@
 import 'materialize-css';
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
+
+import { HOME_ROUTE } from 'core/utils/routes';
 
 import Header from './Header';
 
-describe('Header component tests', () => {
-	test('Should render NavBar', () => {
-		const history = createMemoryHistory();
-		history.push('/users');
+function renderHeader() {
+	const history = createMemoryHistory();
 
-		render(
-			<Router history={history}>
-				<Header />
-			</Router>
-		);
+	render(
+		<Router history={history}>
+			<Header />
+		</Router>
+	);
+
+	return history;
+}
+
+describe('Header component render tests', () => {
+	test('Should render NavBar', () => {
+		renderHeader();
 
 		const navbar = screen.getByTestId('navbar');
 		
 		expect(navbar).toBeInTheDocument();
 	});
 
-	test('Should not render NavBar', () => {
-		const history = createMemoryHistory();
-		history.push('/');
+	test('Should render app logo', () => {
+		renderHeader();
 
-		render(
-			<Router history={history}>
-				<Header />
-			</Router>
-		);
+		const appLogo = screen.getByTestId('app-logo');
 
-		const navbar = screen.queryByTestId('navbar');
-
-		expect(navbar).not.toBeInTheDocument();
+		expect(appLogo).toBeInTheDocument();
 	});
 
+	test('Should render account nav item', () => {
+		renderHeader();
+
+		const accountNavItem = screen.getByTestId('user-account');
+
+		expect(accountNavItem).toBeInTheDocument();
+	});
+
+	test('Should render more options nav item', () => {
+		renderHeader();
+
+		const moreOptionsNavItem = screen.getByTestId('more-options');
+
+		expect(moreOptionsNavItem).toBeInTheDocument();
+	});
 });
+
+const leftClick = { button: 0 };
+
+describe('Header component routes tests', () => {
+	test('Should navigate to home scrren', () => {
+		const history = renderHeader();
+
+		const appLogo = screen.getByTestId('app-logo');
+
+		userEvent.click(appLogo, leftClick);
+
+		expect(history.location.pathname).toBe(HOME_ROUTE);
+	});
+});
+
