a6d876a27de160f456861eb09e0027ce17c8a95e
FlavioLucasFer <flaviolukfer@gmail.com>
2021-08-01 13:40:08 -0300

    Small correction on CNPJ validation.

--- src/core/utils/validateCNPJ.js
+++ src/core/utils/validateCNPJ.js
@@ -1,16 +1,19 @@
 function validateCNPJ(value) {
+	const match = value.toString().match(/\d/g);
+	const numbers = Array.isArray(match) ? match.map(Number) : [];
+
 	if (
-		!value ||
-		value == "00000000000000" ||
-		value == "11111111111111" ||
-		value == "22222222222222" ||
-		value == "33333333333333" ||
-		value == "44444444444444" ||
-		value == "55555555555555" ||
-		value == "66666666666666" ||
-		value == "77777777777777" ||
-		value == "88888888888888" ||
-		value == "99999999999999"
+		!numbers ||
+		numbers == "00000000000000" ||
+		numbers == "11111111111111" ||
+		numbers == "22222222222222" ||
+		numbers == "33333333333333" ||
+		numbers == "44444444444444" ||
+		numbers == "55555555555555" ||
+		numbers == "66666666666666" ||
+		numbers == "77777777777777" ||
+		numbers == "88888888888888" ||
+		numbers == "99999999999999"
 	) {
 		return false;
 	}
@@ -33,9 +36,6 @@ function validateCNPJ(value) {
 		else 
 			return false;
 	}
-
-	const match = value.toString().match(/\d/g);
-	const numbers = Array.isArray(match) ? match.map(Number) : [];
 
 	if (numbers.length !== 14) 
 		return false;
