37cb7ae1528067b1df40a8953c86375ed7592b53
FlavioLucasFer <flaviolukfer@gmail.com>
2021-08-02 11:48:31 -0300

    Collection component

---
+++ src/core/components/Collection.jsx
@@ -0,0 +1,55 @@
+import './css/Collection.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+Collection.propTypes = {
+	items: PropTypes.arrayOf(Object).isRequired,
+	identifier: PropTypes.string.isRequired,
+	onSelect: PropTypes.func,
+};
+
+function Collection(props) {
+	const {
+		items,
+		identifier,
+		onSelect,
+	} = props;
+
+	const [activeItem, setActiveItem] = useState(-1);
+
+	function renderId(item, index) {
+		if (!item.id) {
+			return;
+		}
+
+		return (
+			<span className={`item-id ${!isActive(index) && 'grey-text text-darken-3'}`} >#{item.id}</span>
+		);
+	}
+
+	function isActive(index) {
+		return index === activeItem;
+	}
+
+	function handleListItemClick(index) {
+		setActiveItem(index);
+
+		if (onSelect)
+			onSelect(items[index]);
+	}
+
+	return (
+		<ul className="collection">
+			{items.map((e, i) => (
+				<li key={i} 
+					className={`collection-item ${isActive(i) && 'active'}`}
+					onClick={() => handleListItemClick(i)}>
+					{renderId(e, i)}
+					{e[identifier]}
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default Collection;
